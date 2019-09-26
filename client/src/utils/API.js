030780f478979748dd6e271c0b474aa533cc47cf
Michael Wilkens <43944460+MichaelWilkens@users.noreply.github.com>
2019-09-26 09:56:14 -0700

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 export default {
     artistInfo: function(query){
-        return axios.get(`https://rest.bandsintown.com/artists/${query}?app_id=bit`);
+        return axios.get(`https://rest.bandsintown.com/artists/${query}?app_id=**********`);
     },
   
     events: function(query) {
-    return axios.get(`https://rest.bandsintown.com/artists/${query}/events?app_id=bit`);
+    return axios.get(`https://rest.bandsintown.com/artists/${query}/events?app_id=**********`);
   }
 };
